84695397529a20cd9f28e04d2c84655c74604ceb
Jonas Kell <jonas-kell@web.de>
2023-10-28 20:31:34 +0200

    test: merge-ready

--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -3,12 +3,12 @@ import { operatorFromString } from "./../functions/parser";
 
 describe("parser module end-to-end", () => {
     test("default variable parsing", () => {
-        // expect(() => operatorFromString("asd")).not.toThrow();
-        expect(operatorFromString("asd").exportFormulaString()).toBe("{asda}");
-        // expect(JSON.parse(operatorFromString("asd").serializeStructure())).toMatchObject({
-        //     children: [],
-        //     type: "variable",
-        //     value: "asd",
-        // });
+        expect(() => operatorFromString("asd")).not.toThrow();
+        expect(operatorFromString("asd").exportFormulaString()).toBe("{asd}");
+        expect(JSON.parse(operatorFromString("asd").serializeStructure())).toMatchObject({
+            children: [],
+            type: "variable",
+            value: "asd",
+        });
     });
 });
