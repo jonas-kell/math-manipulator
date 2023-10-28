c758f2a5836026f626133d3ed13ab49298ece52a
Jonas Kell <jonas-kell@web.de>
2023-10-28 20:32:56 +0200

    test: merge-ready

--- src/tests/parser.test.ts
+++ src/tests/parser.test.ts
@@ -4,7 +4,7 @@ import { operatorFromString } from "./../functions/parser";
 describe("parser module end-to-end", () => {
     test("default variable parsing", () => {
         expect(() => operatorFromString("asd")).not.toThrow();
-        expect(operatorFromString("asd").exportFormulaString()).toBe("{asdasd}");
+        expect(operatorFromString("asd").exportFormulaString()).toBe("{asd}");
         expect(JSON.parse(operatorFromString("asd").serializeStructure())).toMatchObject({
             children: [],
             type: "variable",
