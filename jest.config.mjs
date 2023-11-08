f37cddbbf7e8019dd95732f2e4c825abc5b1851a
Jonas Kell <jonas-kell@web.de>
2023-11-08 16:33:54 +0100

    test: do not cover pinia stores

--- jest.config.mjs
+++ jest.config.mjs
@@ -21,7 +21,7 @@ const config = {
     collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    // collectCoverageFrom: undefined,
+    collectCoverageFrom: ["src/functions/*.ts"],
 
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
