e969d4fedc1995d5e34efa5b5ac82803828c4fde
Jonas Kell <jonas-kell@web.de>
2023-11-25 17:35:50 +0100

    feat: hide parts of application on flag

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    const VITE_RENDER_ROUTER_LINKS = import.meta.env.VITE_RENDER_ROUTER_LINKS != "0";
+</script>
 
 <template>
     <h1>Math Manipulator: Help Playground</h1>
 
-    <p>Head Back to the <router-link to="/">Welcome Page</router-link></p>
+    <p v-if="VITE_RENDER_ROUTER_LINKS">Head Back to the <router-link to="/">Welcome Page</router-link></p>
 
     TO BE DONE
 </template>
