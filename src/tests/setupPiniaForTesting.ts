1b184574b267cf8886afcc0fcebb7e5062c8ec75
Jonas Kell <jonas-kell@web.de>
2023-11-27 17:36:44 +0100

    test: tests now correctly use the memory-permanence-storage

--- src/tests/setupPiniaForTesting.ts
+++ src/tests/setupPiniaForTesting.ts
@@ -1,6 +1,10 @@
 import { setActivePinia, createPinia } from "pinia";
+import { PermanenceStorageModes, usePermanenceStore } from "../functions";
 
 // creates a fresh pinia and makes it active
 // so it's automatically picked up by any useStore() call
 // without having to pass it to it: `useStore(pinia)`
-export default () => setActivePinia(createPinia());
+export default () => {
+    setActivePinia(createPinia());
+    usePermanenceStore().setStorageModeTo(PermanenceStorageModes.memory);
+};
