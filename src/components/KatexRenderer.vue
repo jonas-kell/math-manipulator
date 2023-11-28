a4b9b99423c496b7d64d614ac8f2dd9ccb60cf19
Jonas Kell <jonas-kell@web.de>
2023-11-28 01:14:54 +0100

    feat: access macros for symbols that get used by the parser

--- src/components/KatexRenderer.vue
+++ src/components/KatexRenderer.vue
@@ -74,6 +74,17 @@
             // render new content
             katex.render(props.katexInput, element, {
                 macros: {
+                    "\\semicolon": ";",
+                    "\\plus": "+",
+                    "\\minus": "-",
+                    "\\starsymb": "\\ast",
+                    "\\forwardslash": "/",
+                    // "\\backslash": "\\backslash", // already exists per default and breaks if defined
+                    "\\colon": ":",
+                    "\\lbracket": "(",
+                    "\\rbracket": ")",
+                    "\\faculty": "!",
+                    "\\percent": "\\%",
                     "\\eq": "=",
                 },
                 throwOnError: false,
